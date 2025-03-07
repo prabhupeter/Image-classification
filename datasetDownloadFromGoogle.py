4f1ba1d22ee792ec1256f4f9fd6f1124d3bf3239
prabhu teja <prabhuteja240@gmail.com>
2025-03-07 15:32:51 +0530

    Add files via upload

---
+++ datasetDownloadFromGoogle.py
@@ -0,0 +1,44 @@
+from pygoogle_image import image as pi
+
+pi.download("Harry Potter",limit=20)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##from simple_image_download import simple_image_download as sim
+##
+##response = sim.simple_image_download
+##
+##response().download('harrypotter', 60)
+
+#print(response().urls('batmanjoker', 45))
+
+#response().download('batmanjoker', 5,extensions={'.jpg'})
+#print(response().urls('batmanjoker', 5,extensions={'.jpg'})
+
+
+
